8e24b8392da536b5a682f093acce43c1997eac2f
yang1young <1031545274@qq.com>
2017-07-26 23:02:37 +0800

    add main

--- pandas_learn_demo/Pandas.py
+++ pandas_learn_demo/Pandas.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -280,4 +279,5 @@ def file():
                       columns=['A', 'B', 'C', 'D'])
     pd.read_csv('foo.csv')
     df.to_csv('foo.csv')
-
+if __name__ == "__main__":
+   print ''
